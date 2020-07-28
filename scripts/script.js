cd2f5fde26b0aebcaeb9048f28d0478303fd0bfb
rb404 <55969596+rb404@users.noreply.github.com>
2020-07-28 16:29:51 -0400

    Updated webpage layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
-var width = window.innerWidth;
-var height = window.innerHeight;
 var stage;
 var layer;
 var tween;
 var w;
 var h;
+var show_search = false;
+var num_nodes = 0;
+var num_visited = 0;
+var bad_nodes = 0;
+document.getElementById('solve').disabled = true;
 
 // Load all 24 permuations of up, left, down, right from file
 var json = null;
@@ -40,19 +43,26 @@ function set_color(shape, color){
   }, 500);
 }
 function mark_visited(i,j){
+  ++num_visited;
   var shape = stage.find(`#${i}_${j}`)[0];
-  set_color(shape,'red');
+  set_color(shape,'#66FF66');
 }
 
 function mark_unvisited(i,j){
   var shape = stage.find(`#${i}_${j}`)[0];
-  set_color(shape,'white');
+  ++bad_nodes;
+  if(show_search){
+    set_color(shape,'#E12C2C');
+  }
+  else{
+    set_color(shape,'white');
+  }
 }
 
 function connect(i1,j1,i2,j2){
-
   // Find the i,j index of the black wall between the two white nodes
   // and turn it into a non-wall
+  ++num_nodes;
   var i;
   var j;
   if(i1 == i2){
@@ -69,10 +79,13 @@ function connect(i1,j1,i2,j2){
 }
 
 function make_checkerboard(w,h){
+  num_visited = 0;
+  num_nodes = 0;
+  bad_nodes = 0;
   stage = new Konva.Stage({
-    container: 'container',
-    width: width,
-    height: height,
+    container: 'maze',
+    width: 10 * w,
+    height: 10 * h,
   });
   layer = new Konva.Layer();
   for(let i = 0; i < h; ++i){
@@ -82,6 +95,7 @@ function make_checkerboard(w,h){
           if(i % 2 == 1 && j % 2 == 1){
               color = 'white';
               is_wall = false;
+              ++num_nodes;
           }
           var rect = new Konva.Rect({
               x: 10 * j,
@@ -105,22 +119,26 @@ function make_checkerboard(w,h){
   // Make start and end point green and red
   var start = stage.find(`#0_1`)[0];
   var end = stage.find(`#${h-1}_${w-2}`)[0];
-  start.attrs.fill = 'green';
-  end.attrs.fill = 'red';
+  start.attrs.fill = '#66FF66';
+  end.attrs.fill = '#66FF66';
 }
 
+/*
 document.getElementById('reset').addEventListener('click', () => {
   stage.destroy();
   layer.destroy();
 });
-
+*/
 document.getElementById('myForm').addEventListener('submit', (event) =>{
   // Prevent page refresh
   event.preventDefault();
-
+  document.getElementById("correct").innerHTML = null;
+  document.getElementById("incorrect").innerHTML = null;
+  document.getElementById("tot-nodes").innerHTML = null;
+  document.getElementById("efficiency").innerHTML = null;
   // Get width and height input
-  w = $('#width').val();
-  h = $('#height').val();
+  w = 2 * $('#width').val() + 1;
+  h = 2 * $('#height').val() + 1;
   console.log('Generating');
 
   // Make the checkerboard
@@ -128,13 +146,22 @@ document.getElementById('myForm').addEventListener('submit', (event) =>{
   
   // Start at end and generate maze
   //var first = stage.find(`#1_1`)[0];
-  var first = stage.find(`#${h-2}_${w-2 }`)[0];
+  var first = stage.find(`#${h-2}_${w-2}`)[0];
   gen_maze(first);
+  document.getElementById('solve').disabled = false;
 });
 
 document.getElementById('solve').addEventListener('click', () => {
   // Dfs_solve returns whether maze is solvable or not
+  document.getElementById('solve').disabled = true;
+  console.log("SOLVING");
+  show_search = document.getElementById('search-space').checked ? true : false;
   if(!dfs_solve(1,1,w,h)){
     console.log("Unsolvable");
   }
+  let rate = (num_visited/num_nodes) * 100;
+  document.getElementById("tot-nodes").innerHTML = "Total Nodes: " + num_nodes;
+  document.getElementById("correct").innerHTML = "Correct Nodes: " + (num_visited - bad_nodes);
+  document.getElementById("incorrect").innerHTML = "Inorrect Nodes: " + bad_nodes;
+  document.getElementById("efficiency").innerHTML = "Total Maze Traversed: " + rate + '%';
 },false);
